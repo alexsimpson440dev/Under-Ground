633e52958e0a4602b905594e01b91c114e9a33de
Alexander Simpson <alexander.simpson@target.com>
2018-02-28 12:56:05 -0600

    Finished the bill_account class and added it to the database file for mapping.

--- src/tables/table_bill_account.py
+++ src/tables/table_bill_account.py
@@ -1,6 +1,6 @@
 class BillAccount(object):
-    def __init__(self, account_name, account_type, manager_id, account_id):
+    def __init__(self, manager_id, account_name, account_type, account_id=None):
         self.account_id = account_id
+        self.manager_id = manager_id
         self.account_name = account_name
         self.account_type = account_type
-        self.manager_id = manager_id
