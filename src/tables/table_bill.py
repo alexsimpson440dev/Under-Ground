57b4a3d232dbb35027bc5d0dd1832f041a999031
Alexander Simpson <alexsimpson440dev@gmail.com>
2018-03-01 21:43:59 -0600

    Added bill table.

---
+++ src/tables/table_bill.py
@@ -0,0 +1,15 @@
+class Bill(object):
+    def __init__(self, account_id, bill_config_id, date, bill_c_1, bill_c_2, bill_c_3, bill_c_4, bill_c_5,
+                 total_pp, total, due_date, bill_id=None):
+        self.bill_id = bill_id
+        self.account_id = account_id
+        self.bill_config_id = bill_config_id
+        self.date = date
+        self.bill_c_1 = bill_c_1
+        self.bill_c_2 = bill_c_2
+        self.bill_c_3 = bill_c_3
+        self.bill_c_4 = bill_c_4
+        self.bill_c_5 = bill_c_5
+        self.total_pp = total_pp
+        self.total = total
+        self.due_date = due_date
