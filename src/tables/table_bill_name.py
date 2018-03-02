9a9203f91a0aa7a25e98e6643e3a83a020dae3d2
Alexander Simpson <alexsimpson440dev@gmail.com>
2018-03-01 21:29:01 -0600

    Added PK to bill name table.

--- src/tables/table_bill_name.py
+++ src/tables/table_bill_name.py
@@ -1,5 +1,6 @@
 class BillName(object):
-    def __init__(self, account_id, bill_1, bill_2, bill_3, bill_4, bill_5):
+    def __init__(self, account_id, bill_1, bill_2, bill_3, bill_4, bill_5, bill_config_id=None):
+        self.bill_config_id = bill_config_id
         self.account_id = account_id
         self.bill_1 = bill_1
         self.bill_2 = bill_2
